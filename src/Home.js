b8038348d306f29d8476860294ab46e9f8f822c6
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2021-02-25 10:48:06 +0100

    Removed the Footer component from the Home component

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import EditForm from './components/EditForm/EditForm';
-import Footer from './components/Footer/Footer';
+// import Footer from './components/Footer/Footer';
 import Form from './components/Form/Form';
 import InfoPreview from './components/InfoPreview/InfoPreview';
 import Record from './components/Record/Record';
